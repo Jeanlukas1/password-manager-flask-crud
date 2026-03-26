474b6f888e47df8c4bd212b041fb2096fe173f1a
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2026-03-26 11:57:18 -0300

    Removes unused routes and error handler
    Simplifies the application by removing the `/hello` route and the custom 404 error handler, as these functionalities are no longer required.

--- app.py
+++ app.py
@@ -3,14 +3,7 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name=None):
-    return render_template("hello.html", name=name)
-    
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template("page_not_found.html"), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
