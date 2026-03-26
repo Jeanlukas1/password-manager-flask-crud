33f6bdc6eda81b96d6c777ea57811dafafe0d939
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2026-03-26 14:32:30 -0300

    Adds explanatory comments to API endpoints
    Enhances code clarity by adding detailed comments to the `create_password` and `list_passwords` functions.
    
    Comments in `create_password` explain the rationale for using separate `if` statements for independent validations.
    
    Comments in `list_passwords` clarify the use of list comprehension and the necessity of converting objects to dictionaries for JSON serialization.

--- app.py
+++ app.py
@@ -9,9 +9,14 @@ id_control = 1
 
 @app.route("/passwords", methods=["POST"])
 def create_password():
+    """if isolado pois estamos fazendo uma validação isolada da outra, caso o contrário o elif seria mais apropriado
+    por exemplo caso eu quisesse que uma das validações abaixo fosse true, porém eu quero que todas sejam checadas
+        Situação	                    Use
+    Validações independentes	      if separados 
+    Decisão única (um ou outro)	  elif """
     global id_control
     data = request.get_json()
-
+    
     if not isinstance(data, dict):
         return jsonify({"error": "JSON deve ser um objeto"}), 400
 
@@ -32,10 +37,13 @@ def create_password():
 
 @app.route("/passwords", methods=["GET"])
 def list_passwords():
+    """List-comprehention Expressão/oque eu quero que esta variavel temporaria faça ou gere, no caso a variavel temporaria
+    percorrera cada objeto e formatará cada objeto dentro de passowords. o to_dict() é como se fosse uma variável de 
+    apresentação porém em formato dicionario pois precisamos converter este objeto em um dicionário depois para Json
+    pois não podemos utilizar objetos para trafegar dados"""
     if not passwords:
         return jsonify({"Message": "Lista Vazia, adicione uma tarefa na sua lista"}), 404
     return jsonify([p.to_dict() for p in passwords]), 200
-
 
 if __name__ == "__main__":
     app.run(debug=True)
