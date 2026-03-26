2cf3ca3a33b9d49c6a8b5b96da58d3fe49a88b67
Jean Lukas de Marins Costa  do Nascimento <lukasjean745@gmail.com>
2026-03-26 13:29:38 -0300

    Adds password creation endpoint and model
    Introduces a new `Password` model to structure password data.
    Implements a POST `/passwords` endpoint to allow users to create and store new password entries.
    Includes validation for required fields (`name`, `password`) to ensure valid data submission.
    This establishes the foundation for managing password resources within the application.

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request, jsonify
+from models.password import Password
 
 app = Flask(__name__)
 
+passwords = []
+id_control = 1
 
+
+@app.route("/passwords", methods=["POST"])
+def create_password():
+    global id_control
+    data = request.get_json()
+
+    if not isinstance(data, dict):
+        return jsonify({"error": "JSON deve ser um objeto"}), 400
+
+    if "name" not in data or "password" not in data:
+        return jsonify({"error": "Campos obrigatórios faltando"}), 400
+
+    if data["name"] == "" or data["password"] == "":
+        return jsonify({"message": "Campos obrigatórios não preenchidos!"}), 400
+
+    new_password = Password(
+        id=id_control,
+        name=data["name"],
+        password=data["password"]
+    )
+    id_control += 1
+    passwords.append(new_password)
+    return jsonify({"message": "password succefully created and added to your passwords list"}), 200
+
+@app.route("/passwords", methods=["GET"])
+def list_passwords():
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ models/password.py
@@ -0,0 +1,5 @@
+class Password:
+    def __init__(self, id:int, name:str, password) -> None:
+        self.id = id,
+        self.name = name,
+        self.password = password
